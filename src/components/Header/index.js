9890f9a55d724b0e53439f9c916e7e4a1c13d777
Iago Martins <53104712+iagomartins@users.noreply.github.com>
2020-10-08 17:49:20 -0300

    First commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles.css";
+
+// Creates the HTML object to be rendered.
+const Header = () => <header id="main-header">Learning React</header>;
+
+export default Header;
